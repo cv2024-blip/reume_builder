3303c97cafb51d00aafd88862894359c19a357a2
mido761 <65832996+mido761@users.noreply.github.com>
2024-10-09 17:00:49 +0300

    Update homepage.js

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,30 +1,30 @@
-import { HfInference } from '@huggingface/inference';
-import dotenv from 'dotenv';
-dotenv.config()
-const inference = new HfInference(process.env.HUGGING_FACE_API_KEY);
-let output = ""
+// import { HfInference } from '@huggingface/inference';
+// import dotenv from 'dotenv';
+// dotenv.config()
+// const inference = new HfInference(process.env.HUGGING_FACE_API_KEY);
+// let output = ""
 
-let result = ""
+// let result = ""
 
-let input = ""
-export const fetchInference = async function (inputElem ,outputElem, AssistantMessage) {
-    try {
-        outputElem = ''; // Clear previous 
-        input = inputElem + "\n" + AssistantMessage 
-        console.log("the user message is: " + input)
-        for await (const chunk of inference.chatCompletionStream({
-            model: "tiiuae/falcon-7b-instruct",
-            messages: [{ role: "user", content: input}],
-            max_tokens: 95,
-        })) {
-            output = chunk.choices[0]?.delta?.content || "";
-            outputElem += output.replace("User", "").replace(" summary", "").replace(":", "").replace(/[\r\n]+/g, '');
+// let input = ""
+// export const fetchInference = async function (inputElem ,outputElem, AssistantMessage) {
+//     try {
+//         outputElem = ''; // Clear previous 
+//         input = inputElem + "\n" + AssistantMessage 
+//         console.log("the user message is: " + input)
+//         for await (const chunk of inference.chatCompletionStream({
+//             model: "tiiuae/falcon-7b-instruct",
+//             messages: [{ role: "user", content: input}],
+//             max_tokens: 95,
+//         })) {
+//             output = chunk.choices[0]?.delta?.content || "";
+//             outputElem += output.replace("User", "").replace(" summary", "").replace(":", "").replace(/[\r\n]+/g, '');
             
-        }
-        return outputElem
+//         }
+//         return outputElem
 
-    } catch (error) {
-        console.error("Error during inference:", error);
-        outputElem = "Error occurred during inference.";
-    }
-}
+//     } catch (error) {
+//         console.error("Error during inference:", error);
+//         outputElem = "Error occurred during inference.";
+//     }
+// }
